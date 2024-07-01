543954d3b5a649c569fa30b1fc3bb271c38d95e6
pyhilandjy <hilandjy@gmail.com>
2024-07-01 14:54:24 +0900

    user.py 코드정리

--- app/services/users.py
+++ app/services/users.py
@@ -1,7 +1,7 @@
 from typing import Dict
 import jwt
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from supabase import create_client, Client
 
@@ -12,14 +12,6 @@ router = APIRouter()
 supabase: Client = create_client(settings.supabase_url, settings.supabase_service_key)
 
 JWT_AUDIENCE = "authenticated"
-
-
-# def get_user_id_from_token(token: str) -> str:
-#     try:
-#         payload = jwt.decode(token, settings.supabase_jwt_key, algorithms=["HS256"])
-#         return payload.get("id")
-#     except jwt.PyJWTError:
-#         raise HTTPException(status_code=403, detail="Could not validate credentials")
 
 
 def get_user_info_from_token(token: str) -> str:
