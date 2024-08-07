c60c4249d92fbcd50ae8113510ff5580b0d87c69
pyhilandjy <hilandjy@gmail.com>
2024-08-07 11:35:49 +0900

    test: 프로메테우스 그라파나 테스트 및 cors 추가

--- app/main.py
+++ app/main.py
@@ -1,20 +1,38 @@
 import logging
 from contextlib import asynccontextmanager
-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import audio, reports, stt, users
+from app.routers import audio, reports, stt, users, plan
 from app.services.audio import download_and_process_file
 
-logging.basicConfig(level=logging.INFO)
+# 로그 설정
+log_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+log_file = "app.log"
+
+# 파일 핸들러
+file_handler = logging.handlers.TimedRotatingFileHandler(
+    log_file, when="midnight", interval=1, backupCount=30
+)
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.INFO)
+
+# 콘솔 핸들러
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+console_handler.setLevel(logging.INFO)
+
+# 로거 설정
+logging.basicConfig(level=logging.INFO, handlers=[file_handler, console_handler])
 logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    scheduler = AsyncIOScheduler(timezone=f"Asia/Seoul")
+    scheduler = AsyncIOScheduler(timezone="Asia/Seoul")
     scheduler.add_job(func=download_and_process_file, trigger="interval", minutes=10)
     scheduler.start()
     yield
@@ -25,7 +43,11 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://clab-admin.vercel.app",
+        "https://clab-fe.vercel.app",
+        "http://localhost:3001",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -33,15 +55,15 @@ app.add_middleware(
 
 app.include_router(audio.router, prefix="/audio")
 app.include_router(users.router)
-# app.include_router(files.router, prefix="/files")
 app.include_router(stt.router, prefix="/stt")
 app.include_router(reports.router)
+app.include_router(plan.router)
 
 # depends api key
 # app.include_router(audio.router, prefix="/audio", dependencies=[Depends(get_api_key)])
-# app.include_router(users.router, prefix="/users", dependencies=[Depends(get_api_key)])
-# app.include_router(files.router, prefix="/files", dependencies=[Depends(get_api_key)])
+# app.include_router(users.router, dependencies=[Depends(get_api_key)])
 # app.include_router(stt.router, prefix="/stt", dependencies=[Depends(get_api_key)])
+# app.include_router(reports.router, dependencies=[Depends(get_api_key)])
 
 
 @app.get("/")
