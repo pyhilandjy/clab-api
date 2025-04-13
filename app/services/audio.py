ead1824aefe6469be261215df190b856e179352a
pyhilandjy <hilandjy@gmail.com>
2025-04-13 18:21:41 +0900

    test: test

--- app/services/audio.py
+++ app/services/audio.py
@@ -466,6 +466,3 @@ async def select_audio_info(id: str):
     audio_file = execute_select_query(query=SELECT_AUDIO_FILE, params={"id": id})
     record_time = audio_file[0].record_time
     return record_time
-
-
-# test
